a062509eda8c8b9d68898134a8881670d9303c2e
Amanda Punsammy <amanda.punsammy@gmail.com>
2017-04-28 16:32:56 -0400

    text change on click

--- js/main.js
+++ js/main.js
@@ -8,6 +8,12 @@ $('button.switch').on('click', function() {
     $("body").toggleClass( "light" );
     $("body").toggleClass( "dark" );
 
+    if ( $(this).hasClass("off") ) {
+      $('h1.status').text("Hey, who turned off the lights")
+    }
+    else {
+      $('h1.status').text("It's so bright in here!")
+    }
   });
 
 
